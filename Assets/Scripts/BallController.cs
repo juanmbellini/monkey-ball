50adfc7453051e592a91f2d2476e015b85121279
Facundo Gonzalez Fernandez <fgonzalez@itba.edu.ar>
2018-09-17 18:12:51 -0300

    Add basic death logic

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+	private bool isAlive;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		isAlive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAlive && transform.position.y < -10.0f)
+		{
+			Debug.Log("You lose");
+			isAlive = false;
+		}
+	}
+}
