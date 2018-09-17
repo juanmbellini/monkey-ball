fcf935334e1d2800b60416d127685296559baa0a
Facundo Gonzalez Fernandez <fgonzalez@itba.edu.ar>
2018-09-17 18:07:03 -0300

    Add initial camara follow movement

---
+++ Assets/Scripts/CamaraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour {
+	public Transform ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y + 5,
+			ball.transform.position.z - 7);
+		transform.LookAt(ball);
+	}
+}
