893f43b6861ec94c96e9cc29fb1be32f78d869f7
CyranoRock <amoragues@itba.edu.ar>
2018-10-03 13:38:23 -0300

    Merge dev branch

---
+++ Assets/Scripts/LevelGoal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Sphere")
+		{
+			Debug.Log("Win");
+		}
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	public GameController GM;
+	[SerializeField] private GameObject pausePanel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//--------------------------------------------------------------------------
+		// Game Settings Related Code
+
+		GM = GameObject.Find("GameController").GetComponent<GameController>();
+		pausePanel = GameObject.Find("PauseMenuPanel").GetComponent<GameObject>();
+		pausePanel.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		ScanForKeyStroke();
+	}
+
+	void ScanForKeyStroke()
+	{
+		if(Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			if (!pausePanel.activeInHierarchy) 
+			{
+				PauseGame();
+			}
+			if (pausePanel.activeInHierarchy) 
+			{
+				ContinueGame();   
+			}
+		}
+	}
+
+	private void PauseGame()
+	{
+		Time.timeScale = 0;
+		pausePanel.SetActive(true);
+		//Disable scripts that still work while timescale is set to 0
+	} 
+	private void ContinueGame()
+	{
+		Time.timeScale = 1;
+		pausePanel.SetActive(false);
+		//enable the scripts again
+	}
+}
